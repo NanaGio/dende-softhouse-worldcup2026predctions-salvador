a18109de45ac340e710108bfb3d175ef40d9313a
brguzz <gustavoguzz@outlook.com.br>
2026-06-02 21:47:36 -0300

    correções pontuais no arquivo preditct_winner.py

--- src/predict_winner.py
+++ src/predict_winner.py
@@ -3,50 +3,53 @@ import pandas as pd
 from dende_train import load_and_split_data, train_models
 
 if __name__ == "__main__":
-    #Importando CSVs
+    # Importando CSVs
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    
+    # Lendo o Train Engineered
     train_data_path = os.path.join(base_dir, 'data', 'train_engineered.csv')
-    test_clean_path = os.path.join(base_dir, 'data', 'test_clean.csv')
+    
+    
+    test_data_path = os.path.join(base_dir, 'data', 'test_engineered.csv')
     test_original_path = os.path.join(base_dir, 'data', 'test.csv')
 
-    #Estabelecendo o split
+    # Estabelecendo o split e treinando
     X_train, X_test, y_train, y_test = load_and_split_data(train_data_path)
     trained_models = train_models(X_train, y_train)
 
-    #Escolhendo qual modelo usar, julgamos um modelo de árvore de decisão como o mais preciso para o objetivo final,
-    #já que a lógica de négocio aqui, é separar vitórias e contabilizar o mais provável de vencer.
+    # Escolhendo o Random Forest
     best_model = trained_models['Random Forest (Tuning)']
 
-    # Lendo os CSVs
-    df_test_features = pd.read_csv(test_clean_path)
-
-    # Aqui nós carregamos o CSV original, apenas para resgatar a coluna de nome dos times, que foi descartada no treino do modelo.
+    # Lendo os CSVs de Teste
+    df_test_features = pd.read_csv(test_data_path)
     df_test_original = pd.read_csv(test_original_path)
 
-    #Retornando as colunas retiradas
+    # Retornando as colunas retiradas (apenas por segurança)
     missing_cols = set(X_train.columns) - set(df_test_features.columns)
     for c in missing_cols:
         df_test_features[c] = 0
-
 
     df_test_features = df_test_features[X_train.columns]
 
     # Fazendo a probabilidade
     win_probabilities = best_model.predict_proba(df_test_features)[:, 1]
 
-    # Definimos os nomes junto com a probabilidade de vencer, já que o dataset original foi carregado denovo, e está separado
+    # Juntando com os nomes dos times
     results_df = pd.DataFrame({
         'Team': df_test_original['team_name'],
         'Win_Probability_Percentage': win_probabilities * 100
     })
 
-    # Ranking maior para o menor
-    ranking = results_df.sort_values(by='Win_Probability_Percentage', ascending=False).reset_index(drop=True)
+    # Agrupa por país e tira a média das probabilidades
+    team_avg_prob = results_df.groupby('Team', as_index=False)['Win_Probability_Percentage'].mean()
 
-    # Printando ranking
+    # Ranking ordenado do maior para o menor agora com as médias
+    ranking = team_avg_prob.sort_values(by='Win_Probability_Percentage', ascending=False).reset_index(drop=True)
+
+    # Printando ranking (Agora mostrará 10 países ÚNICOS)
     print("\n" + "-"*50)
-    print(" ★ RANKING DE PROBABILIDADE DE VENCER A COPA ★")
-    print(ranking.head(10)) # Mostra o Top 10
+    print(" ★ RANKING DE PROBABILIDADE MÉDIA DE VENCER A COPA ★")
+    print(ranking.head(10)) 
 
     # Vencedor!!
     champion = ranking.iloc[0]['Team']
